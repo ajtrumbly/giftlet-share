774a9499b41119edcd54d24bf49094cc41849270
Austin Trumbly <137444367+ajtrumbly@users.noreply.github.com>
2024-10-29 06:14:57 -0500

    Update share.js

--- share.js
+++ share.js
@@ -1,1 +1,15 @@
+function getQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    const store = params.get('store');
+    const balance = params.get('balance');
+    
+    if (store && balance) {
+        const details = document.getElementById('cardDetails');
+        details.innerHTML = `
+            <div class="store-name">${store}</div>
+            <div class="balance">$${parseFloat(balance).toFixed(2)}</div>
+        `;
+    }
+}
 
+window.onload = getQueryParams;
